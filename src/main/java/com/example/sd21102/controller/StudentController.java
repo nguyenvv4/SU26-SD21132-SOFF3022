2547ac0f0948e52d306fdec46c71441ecb06355e
nguyenvv4 <nguyenvv4@fpt.edu.vn>
2026-05-19 14:58:11 +0700

    update

--- src/main/java/com/example/sd21102/controller/StudentController.java
+++ src/main/java/com/example/sd21102/controller/StudentController.java
@@ -4,6 +4,7 @@ import com.example.sd21102.model.Student;
 import com.example.sd21102.repo.StudentRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import java.util.List;
@@ -15,13 +16,23 @@ public class StudentController {
     StudentRepo studentRepo;
 
     @GetMapping("/student")
-    public String findAll() {
+    public String findAll(Model model) {
 
         List<Student> students = studentRepo.findAll();
         for (Student student : students) {
             System.out.println(student.toString());
         }
-        return null;
+        model.addAttribute("students", students);
+
+        return "student.html";
+    }
+
+    @GetMapping("/detail")
+    public String getStudent(Model model) {
+        Student student = new Student(1, "SD1123", "nguyenvv4@fpt.edu.vn", "UDPM", "Vu Van Nguyen", "0987654321", "PH1123");
+        model.addAttribute("s", student);
+        System.out.println(student.toString());
+        return "student.html";
     }
 
 }
